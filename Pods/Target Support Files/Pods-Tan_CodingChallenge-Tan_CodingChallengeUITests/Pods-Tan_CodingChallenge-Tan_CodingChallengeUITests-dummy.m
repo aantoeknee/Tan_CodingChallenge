808fdff7d09a5544369120a50efa624a13b6caf4
Anthony Tan <atan@noteefied.org>
2020-12-09 02:17:25 +0800

    Update gitignore file.

---
+++ Pods/Target Support Files/Pods-Tan_CodingChallenge-Tan_CodingChallengeUITests/Pods-Tan_CodingChallenge-Tan_CodingChallengeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tan_CodingChallenge_Tan_CodingChallengeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Tan_CodingChallenge_Tan_CodingChallengeUITests
+@end
